fdb2d472b726aaa02c30c7477a90e62d4437752e
Rika Voice <skyvory@hotmail.com>
2017-11-23 16:25:56 +0800

    Update add-on to WebExtension
    Working alpha of Frame Demolition webextension

---
+++ dewey-content-script.js
@@ -0,0 +1,224 @@
+var download_element = '\
+	<div class="frame-demolition">\
+		<button class="fd-download-button waves-effect waves-light btn pink" id="fddownload"><i class="material-icons left"></i>Download All</button>\
+		<span class="fd-download-status">notice: your browser will stop responding for up to a minute when downloading</span>\
+		<div class="fd-download-loading-bar progress light-green" style="display:none;">\
+			<div class="indeterminate"></div>\
+		</div>\
+	</div>\
+';
+$(download_element).appendTo(".detailright");
+
+var jumpto_element = '\
+	<button class="fd-jump-to-download-button waves-effect waves-light btn teal" id="fdjumptodownload">to download</button>\
+';
+$(jumpto_element).appendTo('.detailleft');
+
+document.getElementById("fdjumptodownload").addEventListener("click", function() {
+	var container = $('html,body');
+	var scrollto = $('.fd-download-button');
+	container.animate({
+		scrollTop: scrollto.offset().top - container.offset().top - 300,
+		srollLeft: 0
+	}, 300);
+});
+
+var target_element = document.getElementById("fddownload");
+target_element.addEventListener("click", function() {
+	// show download loading bar
+	$('.fd-download-loading-bar').fadeIn();
+	// disable download button
+	$('.fd-download-button').addClass("disabled");
+	// begin file downloading and processing
+	downloadFile(fileUrls[count], onDownloadComplete);
+});
+
+
+// debug button ---start---
+// var debug_element = '<div class="frame-demolition-debug"><button class="fd-debug-button" id="fddebug">Debug</button></div>';
+// $(debug_element).appendTo(".detailright");
+
+// var debug_target = document.getElementById("fddebug");
+// debug_target.addEventListener("click", function() {
+// 	console.log("DEBUG");
+// 	var section_list = $('.detailright ol li'), individual_section, section_link, links = [];
+// 	for(var i = 0; i < section_list.length; i++) {
+// 		individual_section = $('.detailright ol li a:eq(' + i + ')');
+// 		section_link = individual_section[0].getAttribute("href");
+// 		console.log(section_link);
+// 		section_link = section_link.replace("ft_viewer.php?fname=", "");
+// 		section_link = "http://dewey.petra.ac.id/repository/jiunkpe/" + section_link;
+// 		links.push(section_link);
+// 	}
+// 	console.log(links);
+// });
+// debug button ---end---
+
+// direct link example to dewey pdf
+// 'http://dewey.petra.ac.id/repository/jiunkpe/jiunkpe/s1/info/2010/jiunkpe-ns-s1-2010-26407079-19762-cosine-cover.pdf', 
+// http://dewey.petra.ac.id/repository/jiunkpe/jiunkpe/s1/info/2010/jiunkpe-ns-s1-2010-26407079-19762-cosine-chapter1.pdf
+
+
+// collecting and parsing pdf links
+var section_list = $('.detailright ol li'), individual_section, section_link, links = [];
+var extension_regex = /(?:\.([^.]+))?$/;
+
+for(var i = 0; i < section_list.length; i++) {
+	// get item list according order
+	individual_section = $('.detailright ol li a:eq(' + i + ')');
+	// get link value of href
+	section_link = individual_section[0].getAttribute("href");
+	// replace unnecessary string from the link
+	var extension = extension_regex.exec(section_link)[1];
+	if(extension == "pdf") {
+		section_link = section_link.replace("ft_viewer.php?fname=", "");
+		// concat path to direct pdf file
+		section_link = "http://dewey.petra.ac.id/repository/jiunkpe/" + section_link;
+	}
+	else if(extension == "jpg") {
+		section_link = section_link;
+	}
+	else {
+		section_link = null;
+	}
+	if(section_link) {
+		// append parsed link to array
+		links.push(section_link);
+	}
+}
+
+// var fileUrls = ['http://www.w3schools.com/images/w3schools.png', 'http://dewey.petra.ac.id/repository/jiunkpe/jiunkpe/s1/info/2010/jiunkpe-ns-s1-2010-26407079-19762-cosine-conclusion.pdf', 'http://dewey.petra.ac.id/repository/jiunkpe/jiunkpe/s1/info/2010/jiunkpe-ns-s1-2010-26407079-19762-cosine-chapter1.pdf'];
+// var fileUrls = ['http://dewey.petra.ac.id/repository/jiunkpe/jiunkpe/s1/jdkv/2009/jiunkpe-ns-s1-2009-42405108-14162-jasa-extras1.jpg'];
+var fileUrls = links;
+var zip = new JSZip();
+var count = 0;
+
+// downloadFile(fileUrls[count], onDownloadComplete);
+
+function downloadFile(url, onSuccess) {
+	// change status
+	var downloading_file_name = fileUrls[count].substring(fileUrls[count].lastIndexOf('/') + 1);
+	$('.fd-download-status').text("downloading " + downloading_file_name + " ...");
+
+	// with native xhr
+	// var xhr = new XMLHttpRequest();
+	// // xhr.onprogress = calculateAndUpdateProgress();
+	// xhr.open("GET", url, true);
+	// // xhr.responseType = 'blob';
+	// xhr.responseType = 'arrayBuffer';
+	// xhr.onreadystatechange = function() {
+	// 	console.log(xhr.readyState);
+	// 	if(xhr.readyState == 4) {
+	// 		if(onSuccess) {
+	// 			console.log("XHR RESOPNSE", xhr.response);
+	// 			// var blob = new Blob(xhr.response, {type: "application/pdf"});
+	// 			onSuccess(xhr.response);
+	// 			// onSuccess(blob);
+	// 			// console.log("BLOB", blob);
+	// 		}
+	// 	}
+	// }
+	// // xhr.onload = function(event) {
+	// // 	var blob = xhr.response;
+	// // 	console.log(blob);
+	// // }
+	// xhr.send();
+
+	// with jquery
+	// var request = $.ajax({
+	// 	url: url,
+	// 	type: "GET",
+	// 	contentType: "application/pdf",
+	// 	mimeType: 'text/plain; charset-x-user-defined',
+	// });
+
+	// request.done(function(data) {
+	// 	console.log("JQUERY XHR RESPONSE", data);
+	// 	onSuccess(data);
+	// });
+
+
+	// with jszip-util
+	JSZipUtils.getBinaryContent(url, function(error, data) {
+		if(error) {
+			console.log("ERROR", error);
+		}
+		else {
+			onSuccess(data);
+		}
+	});
+}
+
+function onDownloadComplete(blobData) {
+	if(count < fileUrls.length) {
+		blobToBase64(blobData, function(binaryData) {
+			// determine name of the file to be added
+			var fileName = fileUrls[count].substring(fileUrls[count].lastIndexOf('/') + 1);
+			// add file to zip (append)
+			zip.file(fileName, binaryData, {binary: true});
+			// zip.file(fileName, binaryData);
+			if(count < fileUrls.length - 1) {
+				count++;
+				downloadFile(fileUrls[count], onDownloadComplete);
+			}
+			else {
+				// all files finished download
+				// ready to zip
+				// change downloding status
+				$('.fd-download-status').text("processing PDFs... please wait until browser is responding again (up to a minute)");
+				// genereate the zip of appended files and put in inside a variable
+				var content = zip.generate();
+
+				var dewey_title = $('#content-wrap #content #main #resultwindow .detailright .title').text();
+				var parsed_dewey_title = dewey_title.replace(/\s/g, "-");
+				var zipName = parsed_dewey_title + ".zip";
+
+				// change downloading status
+				$('.fd-download-status').text("sweet!");
+				// hide downlod loading bar
+				$('.fd-download-loading-bar').fadeOut();
+				// enable download button
+				$('.fd-download-button').removeClass("disabled");
+
+				// return count to 0
+				count = 0;
+
+				// simulate download action towards generated zip of which instance temporarily attached to document
+				var link = document.createElement('a');
+				link.download = zipName;
+				link.href = "data:application/zip;base64," + content;
+				document.body.appendChild(link);
+				link.click();
+				document.body.removeChild(link);
+				delete link;
+			}
+		});
+	}
+}
+
+function blobToBase64(blob, callback) {
+	// toggling false for pdf doesn't need conversion as of arrayBuffer
+	var toggle = false;
+	if(toggle) {
+		var reader = new FileReader();
+		reader.onload = function() {
+			var dataUrl = reader.result;
+			var base64 = dataUrl.split(',')[1];
+			callback(base64);
+		};
+		reader.readAsDataURL(blob);
+	}
+	else {
+		callback(blob);
+	}
+}
+
+function calculateAndUpdateProgress(evt) {
+	if(evt.lengthComputable) {
+		console.log("evt");
+	}
+}
+
+// parse dewey description text
+var detailright_text = $(".detailright p").text();
+$(".detailright p").html(detailright_text);
